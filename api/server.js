48ea3903ac7d64662c739c437160921756471166
Devon Osei <oseidevon@gmail.com>
2021-07-09 18:46:38 -0400

    yurr

--- api/server.js
+++ api/server.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const server = express();
+const Ting = require('./projects-model')
 
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
+
+
+server.use(express.json())
+
+//Endies
+server.get('yoyo', (req, res) =>{
+  res.json({message:'more yo'})
+})
+
+server.get('/api/IDK',(req, res) =>{
+
+  Ting.findById()
+  .then(Tings =>{
+    res.json(Tings)
+  })
+  .catch(err => {
+    res.status(500).json({
+      message:"woah woah woah",
+      error: err.message, })
+  })
+})
 
 module.exports = server;
--- index.js
+++ index.js
@@ -12,3 +12,36 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Pull your server into this file and start it!
 */
+
+// const server = required('/api/projects')
+
+// server.listen(5000,() =>{
+//   console.log('yo mama')
+// })
+
+
+require('dotenv').config()
+const express = require('express')
+const server = express()
+console.log(process.env.USER) // env USER=Dbag
+console.log(process.env.SHELL) // env SHELL=/bin/zhs
+if (process.env.NODE_ENV === 'production') {
+  console.log('this means this code is deployed')
+}
+const PORT = process.env.PORT || 5000
+
+server.use(express.json())
+
+server.use(cors())
+
+server.use('*', (req, res, next) => {
+  res.send(`<h1>Hello, there!</h1>`)
+})
+
+console.log('port is -> ', PORT)
+server.get('/api/projects', (req, res) => {
+  res.json({ message: `${process.env.COHORT} Tinger` })
+})
+server.listen(PORT, () => {
+  console.log(`listening on ${PORT}`)
+})
